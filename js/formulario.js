3a13927ba09e358a4fb34a69173c8a1574b7f4bb
Oly87 <ingfrancisherrera@gmail.com>
2022-06-07 00:30:43 -0300

    Se agrego link al icono del logo y validacion del formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,19 @@
 // Validacion Formulario
+
+function validar(){
+    var nombre, apellido, email, mensaje;
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    email = document.getElementById("email").value;
+    mensaje = document.getElementById("mensaje").value;
+
+    if(nombre === "" || apellido === "" || email === "" || mensaje === ""){
+        alert("All fields are required");
+        return false;
+    }
+    else if (mensaje === ""{
+        alert("Please type a message")
+    })
+    
+}
+
